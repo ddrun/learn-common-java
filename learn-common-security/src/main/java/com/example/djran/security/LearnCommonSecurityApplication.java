737cd261158157243fe95cfc11dda2368ffeaa5f
ddrun <d.djran@gmail.com>
2018-03-24 14:10:01 +0800

    common code  in learning java

---
+++ learn-common-security/src/main/java/com/example/djran/security/LearnCommonSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.example.djran.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnCommonSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnCommonSecurityApplication.class, args);
+	}
+}

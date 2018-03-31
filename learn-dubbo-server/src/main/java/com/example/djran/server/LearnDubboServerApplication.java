79f8cda7c1711ebc25c88e9c38915b21e6220784
邓剑然 <1632976579@qq.com>
2018-03-31 20:09:57 +0800

    dubbo学习demo

---
+++ learn-dubbo-server/src/main/java/com/example/djran/server/LearnDubboServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.djran.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author d.djran@gmail.com
+ */
+@SpringBootApplication
+public class LearnDubboServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnDubboServerApplication.class, args);
+	}
+}

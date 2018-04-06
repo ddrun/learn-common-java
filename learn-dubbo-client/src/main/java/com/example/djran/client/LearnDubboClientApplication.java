06e6875b7b4480a816e89bfad5adc70ee64efd4c
邓剑然 <1632976579@qq.com>
2018-04-06 16:46:23 +0800

    dubbo client demo

---
+++ learn-dubbo-client/src/main/java/com/example/djran/client/LearnDubboClientApplication.java
@@ -0,0 +1,12 @@
+package com.example.djran.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnDubboClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnDubboClientApplication.class, args);
+	}
+}
